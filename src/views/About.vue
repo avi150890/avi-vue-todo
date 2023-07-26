826fd88f754f0de5d1e06bb31c0638137dc3349e
avi150890 <avinash15sharma@gmail.com>
2023-07-26 16:49:44 +0530

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about pa-6">
+    <h1>About {{ $store.state.appTitle }}</h1>
+    <p>
+      Our todo app is a simple and efficient task management tool designed to
+      help users stay organized and focused on their daily responsibilities.
+      With an intuitive and user-friendly interface, users can easily add,
+      prioritize, and manage their tasks. Whether it's personal errands, work
+      assignments, or important reminders, the app allows users to create a
+      comprehensive to-do list to keep track of everything they need to
+      accomplish. Users can mark tasks as completed, set due dates and
+      reminders, and even categorize tasks into different projects or labels for
+      better organization. The app ensures that users never miss a deadline or
+      forget an essential task by sending timely notifications. Our todo app
+      aims to streamline productivity, reduce stress, and empower users to
+      achieve their goals efficiently and effectively
+    </p>
+  </div>
+</template>
